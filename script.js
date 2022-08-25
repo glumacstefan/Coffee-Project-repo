227dee67280402a9d30ca34e3bc21ed0ddceb80a
glumac <stefanactor94@gmail.com>
2022-08-25 23:36:49 +0200

    Transfer the JQ counter inside the JS file

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navLinks.classList.toggle("active");
 })
+
+const backToTop = document.querySelector('.back-to-top');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTop.style.display = "block";
+    }   else {
+        backToTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 50;
+}
+
+jQuery(document).ready(function( $ ) 
+    {
+      $('.counter').counterUp({
+    delay: 10,
+    time: 500
+    });
+    });
+
+
